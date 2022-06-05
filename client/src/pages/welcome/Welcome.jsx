9ad99b164cc6eeb655b539da6549f2128d6ec5f0
DingusDad <the.chris.parisi@gmail.com>
2022-06-05 16:29:30 -0700

    redirect if no user

--- client/src/pages/welcome/Welcome.jsx
+++ client/src/pages/welcome/Welcome.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Welcome = () => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/dashboard');
+    }
+  });
   return <div className="container">Welcome</div>;
 };
 
